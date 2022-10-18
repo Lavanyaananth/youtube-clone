ab1f6d98db05807b2f5513c5a472fb2bcde1912e
lavanyaananth <lavanyaanand.11@gmail.com>
2022-10-18 11:27:15 +0530

    first commit

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useState } from "react-router-dom";
+import { Paper, IconButton } from "@mui/material";
+import { Search } from "@mui/icons-material";
+
+const SearchBar = () => {
+  const selectedCategory = "New";
+  return (
+    <Paper
+      component="form"
+      onSubmit={() => {}}
+      sx={{
+        borderRadius: 20,
+        border: "1px solid #e3e3e3",
+        pl: 2,
+        boxShadow: "none",
+        mr: { sm: 5 },
+      }}
+    >
+      <input
+        className="search-bar"
+        placeholder="Search.."
+        value=""
+        onChange={() => {}}
+      ></input>
+      <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
+        <Search />
+      </IconButton>
+    </Paper>
+  );
+};
+
+export default SearchBar;
